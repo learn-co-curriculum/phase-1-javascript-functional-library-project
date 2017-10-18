86b5e32d3beb6e823723924db571e04e79e9eba6
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-10-18 15:44:46 -0400

    Update indexTest.js

--- test/indexTest.js
+++ test/indexTest.js
@@ -54,7 +54,7 @@ describe('index.js', function () {
 
   describe('reduce', function () {
     const testArr = unmodifiedTestArr.slice() // arr is [1, 2, 3, 4]
-    const callback = (acc, val, collection) => (val * 3) // modified values are [3, 6, 9, 12]
+    const callback = (acc, val, collection) => (acc + (val * 3)
     const reduceSansAcc = fi.reduce(testArr, callback)
     const reduceWithAcc = fi.reduce(testArr, callback, 10)
 
